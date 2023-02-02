d0d9cb646fa3613648f2a3fc548f81c1afbd3035
CCMatson <catherine.matson@gmail.com>
2023-02-01 21:17:54 -0800

    change background

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -5,7 +5,7 @@ function ProjectList (props) {
   return (
     <>
     <div className="project-list">
-      <h1>Projects List</h1>
+      <h1>My Projects</h1>
       <div className="project-preview">
         {/* <ul> */}
           {props.myProjects.map((project, idx) =>
