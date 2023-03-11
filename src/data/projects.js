7cb58ed87ccd8cff755ec2171d02144fdf48cb5d
CCMatson <catherine.matson@gmail.com>
2023-03-11 15:54:35 -0800

    add pokeApi project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,11 @@
 const myProjects = [
   {
-    title: 'Class Cookbook App',
-    technology: "MEN Stack App with MVC architecture",
-    description: '"Class Cookbook" is modern update on the old-fashioned community cookbook. This app is a link collector inspired by Pinterest. Logged in users can add recipe url links to the app, edit and delete recipes, and review other users contributions. This app was created using MEN stack, with database storage supported by mongoose and MongoDB.',
-    image: '/CookBook.png',
-    repositoryLink: 'https://github.com/CCMatson/recipe-collector',
-    deploymentLink: 'https://recipe-collector.fly.dev/'
+    title: 'PokeApi Battle',
+    technology: "React, RESTful Api, HTML, CSS",
+    description: "This React app consumes all of it's data from a RESTful API interface. Users can choose a pokemon and battle against two computer generated opponents. Users can review a comprehensive list of pokemon, and learn specific details about their game play.",
+    image: '/PokiApi.png',
+    repositoryLink: 'https://github.com/CCMatson/pokeapi-react-practice',
+    deploymentLink: 'https://pokeapi-game.netlify.app/'
   },
   {
     title: 'This Portfolio',
@@ -24,12 +24,12 @@ const myProjects = [
     deploymentLink: 'https://shoe-collector.netlify.app/'
   },
   {
-    title: "Undersea Adventure Game",
-    technology: "Vanilla Javascript, CSS, HMTL",
-    description: "A Choose-Your-Own-Adventure style game built to utilize array indexing to advance the story based on the user's choices.",
-    image: '/CYOA.png',
-    repositoryLink: 'https://github.com/CCMatson/choose-your-own-adventure.git',
-    deploymentLink: 'https://ccmatson-choose-your-own-adventure.netlify.app/',
+    title: 'Class Cookbook App',
+    technology: "MEN Stack App with MVC architecture",
+    description: '"Class Cookbook" is modern update on the old-fashioned community cookbook. This app is a link collector inspired by Pinterest. Logged in users can add recipe url links to the app, edit and delete recipes, and review other users contributions. This app was created using MEN stack, with database storage supported by mongoose and MongoDB.',
+    image: '/CookBook.png',
+    repositoryLink: 'https://github.com/CCMatson/recipe-collector',
+    deploymentLink: 'https://recipe-collector.fly.dev/'
   },
   {
     title: 'Fortune Teller App',
@@ -38,7 +38,15 @@ const myProjects = [
     image: '/CosmicCueLanding.png',
     repositoryLink: 'https://github.com/CCMatson/cosmic-cue-front-end',
     deploymentLink: 'https://main--cosmic-cue.netlify.app/'
-  }
+  },
+  {
+    title: "Undersea Adventure Game",
+    technology: "Vanilla Javascript, CSS, HMTL",
+    description: "A Choose-Your-Own-Adventure style game built to utilize array indexing to advance the story based on the user's choices.",
+    image: '/CYOA.png',
+    repositoryLink: 'https://github.com/CCMatson/choose-your-own-adventure.git',
+    deploymentLink: 'https://ccmatson-choose-your-own-adventure.netlify.app/',
+  },
 ]
 
 export default myProjects;
