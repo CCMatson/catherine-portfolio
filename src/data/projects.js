846bd6505639cfd8b09b7033259c4771eb2fa49a
CCMatson <catherine.matson@gmail.com>
2023-04-13 13:59:02 -0700

    update projects page

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,12 +8,12 @@ const myProjects = [
     deploymentLink: 'https://shoe-collector.netlify.app/'
   },
   {
-    title: 'PokeApi Battle',
-    technology: "React, RESTful Api, HTML, CSS",
-    description: "This React app consumes all of it's data from a RESTful API interface. Users can choose a pokemon and battle or view a comprehensive list of pokemon, and learn specific details about their game play.",
-    image: '/PokiApi.png',
-    repositoryLink: 'https://github.com/CCMatson/pokeapi-react-practice',
-    deploymentLink: 'https://pokeapi-game.netlify.app/'
+    title: 'Weather App',
+    technology: "React, TypeScript, Open Weather API, HTML, CSS",
+    description: "As a user, you can input the name of a city to get the weather forecast at that location. This app makes calls to two different API endpoints.",
+    image: '/WeatherApp.png',
+    repositoryLink: 'https://github.com/CCMatson/weather-api-ts',
+    deploymentLink: 'https://weather-api-ts.netlify.app/'
   },
     {
     title: 'This Portfolio',
@@ -24,13 +24,21 @@ const myProjects = [
     deploymentLink: 'https://ccm-react-portfolio.netlify.app/'
   },
   {
-    title: 'Fortune Teller App',
-    technology: "Team project, MERN Stack App",
-    description: 'This app was created by a team of 3 devs, where my role was "git manager" in charge of merging code, and writing the code for the front and back-end of our key data structure. Includes authorization with Google Oath and storage on MongoDB."Fortune Teller" is a positive entertainment app where users can create, edit, and delete fortunes that are later sent to other users who request a "random fortune from the universe"',
-    image: '/CosmicCueLanding.png',
-    repositoryLink: 'https://github.com/CCMatson/cosmic-cue-front-end',
-    deploymentLink: 'https://main--cosmic-cue.netlify.app/'
+    title: 'Plantivity App',
+    technology: "MERN Stack App with JWT auth",
+    description: 'GA Hackathon submission! This app was created by a team of 5 devs and 3 UX/UI designers over four days. My role was to code the font end forms where users input and update their actions. As users progress and check off their actions, the plants in their garden grow. This productivity app encourages balance and positive mental health.',
+    image: '/Plantivity.png',
+    repositoryLink: 'https://github.com/CCMatson/team6-hackathon-fe',
+    deploymentLink: 'https://plantivity.netlify.app/'
   },
+  // {
+  //   title: 'Fortune Teller App',
+  //   technology: "Team project, MERN Stack App",
+  //   description: 'This app was created by a team of 3 devs, where my role was "git manager" in charge of merging code, and writing the code for the front and back-end of our key data structure. Includes authorization with Google Oath and storage on MongoDB."Fortune Teller" is a positive entertainment app where users can create, edit, and delete fortunes that are later sent to other users who request a "random fortune from the universe"',
+  //   image: '/CosmicCueLanding.png',
+  //   repositoryLink: 'https://github.com/CCMatson/cosmic-cue-front-end',
+  //   deploymentLink: 'https://main--cosmic-cue.netlify.app/'
+  // },
   {
     title: 'Class Cookbook App',
     technology: "MEN Stack App with MVC architecture",
