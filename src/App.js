d886cf913162ba98c94228cc7e5f872e4376355f
CCMatson <catherine.matson@gmail.com>
2023-04-22 12:14:54 -0700

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+
 import { Element } from 'react-scroll';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import Projects from './pages/Projects';
 import NavBar from './components/NavBar';
-import ProjectDetails from './pages/ProjectDetails';
+
 
 function App() {
   // const [showDetails, setShowDetails] = useState(false)
